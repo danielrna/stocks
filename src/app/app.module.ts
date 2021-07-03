c156f208dd48280dc5347d60436dc46461aabb92
daniel <daniel.roina@gmail.com>
2021-07-03 23:03:17 +0200

    working v0 with menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {MatIconModule} from "@angular/material/icon";
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {AppComponent} from './app.component';
+import {StockPickerComponent} from './stock-picker/stock-picker.component';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import { HomeComponent } from './home/home.component';
+
+const materialModules = [
+  MatIconModule,
+  MatSnackBarModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatInputModule,
+  MatCardModule,
+  MatProgressSpinnerModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockPickerComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
