76da50d20ddbdf84dd0a0668e2f297838db9648f
daniel <daniel.roina@gmail.com>
2022-01-23 14:20:24 +0100

    fix some formuals

--- src/app/colocation-page/colocation-page.component.ts
+++ src/app/colocation-page/colocation-page.component.ts
@@ -20,16 +20,17 @@ export class ColocationPageComponent implements OnInit {
   copro: number = 0;
   impots: number = 10;
   tf: number = 1500;
-  pno: number = 500;
+  pno: number = 30;
   autre: number = 0;
-  cfe: number = 400;
+  cfe: number = 30;
   entretien: number = 0;
   meubles: number = 10000;
   mensualiteCredit: number = this.PMT();
   gestion: number = 0;
   totalEmprunte: number = 0;
   tfMensuelle: number = 0;
-  totalDepenses: any;
+  monthlyExpenses: any;
+  cashflow: number = 0;
 
   ngOnInit(): void {
     this.calculateAllFields()
@@ -42,41 +43,38 @@ export class ColocationPageComponent implements OnInit {
     this.setMensualiteCredit();
     this.setTfMensuelle()
     this.setNotaireAncien()
-    this.setTotalDepenses()
+    this.setMonthlyExpenses()
+    this.setCashflow()
   }
 
-  setTfMensuelle() {
+  private setTfMensuelle() {
     this.tfMensuelle = this.tf / 12;
   }
 
-  setGestion() {
-    this.gestion = 0.08 * this.monthlyRent;
+  private setGestion() {
+    this.gestion = Math.round(0.08 * this.monthlyRent);
   }
 
-  setTotalEmprunte() {
+  private setTotalEmprunte() {
     this.totalEmprunte = this.travaux + this.meubles + this.price + this.notaire - this.apport;
   }
 
-  setMonthlyRent() {
+  private setMonthlyRent() {
     this.monthlyRent = this.nbroom * this.roomprice
   }
 
-  setNotaireAncien() {
+  private setNotaireAncien() {
     this.notaire = 0.08 * this.price
   }
 
-  setMensualiteCredit() {
+  private setMensualiteCredit() {
     this.mensualiteCredit = this.PMT()
   }
 
   PMT() {
     let rate = this.tauxCredit / 100 / 12
-    console.log("rate=" + rate)
     let nperiod = this.dureeCredit * 12
-    console.log("nperiod=" + nperiod)
     let pv = this.totalEmprunte
-    console.log("pv=" + pv)
-
     let fv = 0;
     let type = 1;
 
@@ -93,7 +91,21 @@ export class ColocationPageComponent implements OnInit {
   }
 
 
-  private setTotalDepenses() {
-    this.totalDepenses = this.copro + this.impots + this.tf + this.pno + this.autre + this.cfe + this.entretien + this.mensualiteCredit
+  private setMonthlyExpenses() {
+    this.monthlyExpenses = this.copro + this.impots + this.tfMensuelle + this.pno + this.autre + this.cfe + this.entretien + this.mensualiteCredit + this.gestion
   }
+
+  private setCashflow() {
+    this.cashflow = this.monthlyRent - this.monthlyExpenses
+  }
+
+  getCashflowClassName() {
+    let color: string = "red"
+    if (this.cashflow > 0) {
+      color = "green"
+    }
+    return "light-" + color + "-input"
+  }
+
+
 }
